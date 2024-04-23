7d3e342ab3f7327196197ed4719002a4fdac7771
StianL82 <stian.lilleng@gmail.com>
2024-04-23 21:26:02 +0200

    Created login and register page and started with js for register page

---
+++ js/index.mjs
@@ -0,0 +1,8 @@
+import { setRegisterFormListener } from "./handlers/register.mjs";
+
+setRegisterFormListener()
+
+
+/* import * as constants from "./api/constants.mjs";
+
+console.log(constants.API_SOCIAL_URL); */
