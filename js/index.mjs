5b07dbc8f32f76972b18954d92016a667803f799
StianL82 <stian.lilleng@gmail.com>
2024-04-24 18:59:10 +0200

    Added create post and update post JS

--- js/index.mjs
+++ js/index.mjs
@@ -1,10 +1,17 @@
 import { setRegisterFormListener } from "./handlers/register.mjs";
 import { setLoginFormListener } from "./handlers/login.mjs";
+import { updatePost } from "./api/posts/update.mjs";
 
 const path = location.pathname;
 
-if (path === '/profile/login/'){
-  setLoginFormListener()
-} else if (path === '/profile/register/') {
-  setRegisterFormListener()
+if (path === "/profile/login/") {
+  setLoginFormListener();
+} else if (path === "/profile/register/") {
+  setRegisterFormListener();
 }
+
+updatePost({
+  id: 11934,
+  title: "Denne eksempelposten har blitt oppdatert",
+  body: "Det har denne bodyen også",
+});
