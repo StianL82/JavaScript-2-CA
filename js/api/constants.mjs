3be3ea6f7cf8337711cdd5b61dc766712fa7c3c0
StianL82 <stian.lilleng@gmail.com>
2024-05-03 18:27:18 +0200

    Update constants.mjs

--- js/api/constants.mjs
+++ js/api/constants.mjs
@@ -1,6 +1,4 @@
 export const API_HOST_URL = "https://api.noroff.dev";
 export const API_BASE = "/api/v1";
 export const API_SOCIAL_BASE = "/social";
-export const API_V2_BASE = "https://v2.api.noroff.dev";
-export const API_KEY_URL = "/auth/create-api-key";
 export const API_SOCIAL_URL = `${API_HOST_URL}${API_BASE}${API_SOCIAL_BASE}`;
